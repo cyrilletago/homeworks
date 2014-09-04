04cc4a10c681ce1a835242215fbf049d67c98e21
cyrilletago <cyrille@aims.edu.gh>
2014-09-04 00:26:05 +0000

    secant

---
+++ secant_meth_ipmlement.py
@@ -0,0 +1,102 @@
+__author__ = 'cryrille'
+from math import *
+from dimension_validate import *
+from numpy import *
+
+
+def secant_method(func, first, second, err):
+    """
+    This function computes the roots of a given function whe given two values,
+    assumed to be close to the root(one higher and one lower)
+    :param func: The function for which the roots are to be calculated.
+    :param first: The fist estimate or starting Guess
+    :param second: The second estimate or other guess
+    :param err: The error
+    :return: root, the root of the function
+    >>> round(secant_method(math.sin, pi/2, 3*pi/2, 1e-6, 6))
+    3.141593
+    """
+
+    if dim_complete(func) and dim_complete(first) and dim_complete(second) and dim_complete(err):
+        if dim_type(first) and dim_type(second) and dim_type(err)and callable(func):
+            if dim_sign(err):
+                x0, x1 = first, second
+                while fabs(func(x1)) > err:
+                    x2 = (x0*func(x1) - x1*func(x0))/(func(x1) - func(x0))
+                    x0 = x1
+                    x1 = x2
+                root = x1
+                return root
+            else:
+                raise ValueError("Please enter positive numbers: "+str(err))
+        raise TypeError("One or more arguments is(are) not number(s)! Enter a Number")
+    raise AttributeError("OH!!! one of the arguments was forgotten, Check it!")
+
+
+def sine(x):
+    return sin
+print(secant_method(sin, pi+1, pi-1, 1e-6))
+
+
+
+def trapezoid_method(func, start_point, end_point, count):
+    """
+    This computes the integral of a given function from start_point to end_point.
+    :param func: The function whose integral is required
+    :param start_point:The lower limit  of the integral
+    :param end_point: The upper limit of the integral
+    :param count: The number of sections we want to divide the graph into
+    :return: The integral of function entered
+    """
+    if dim_complete(func) and dim_complete(start_point) and dim_complete(end_point) and dim_complete(count):
+        if dim_type(start_point) and dim_type(end_point) and dim_type(count) and callable(func):
+            if dim_sign(count):
+                step = (end_point - start_point)/count
+                x1 = start_point
+                x2 = x1 + step
+                temp = 0
+                for i in range(1, count + 1):
+                    temp += func(x2) + func(x1)
+                    x1 = x2
+                    x2 = x2 + step
+                return step*temp/2.0
+            else:
+                raise ValueError("Please enter positive numbers: "+str(count))
+        raise TypeError("One or more arguments is(are) not number(s)! Enter a Number")
+    raise AttributeError("OH!!! one of the arguments was forgotten, Check it!")
+
+"""
+def linear(x):
+    return 1
+print(trapezoid_method(linear, 0, 5, 5))
+"""
+
+
+def midpoint_method(func, lower_limit, upper_limit, count):
+    h = (upper_limit - lower_limit)/count
+    x = linspace(lower_limit, upper_limit, True)
+    midpoints_array = [0]*count
+    summ = 0
+    for i in range(1, count + 1):
+        midpoints_array[i-1] = (x[i] + x[i-1])/2
+        summ += func(midpoints_array[i-1])
+    if dim_complete(func) and dim_complete(upper_limit) and dim_complete(upper_limit) and dim_complete(count):
+            if dim_type(lower_limit) and dim_type(upper_limit) and dim_type(count) and callable(func):
+                if dim_sign(count):
+                    h = (upper_limit - lower_limit)/count
+                    x = linspace(lower_limit, upper_limit, True)
+                    midpoints_array = [0]*count
+                    summ = 0
+                    for i in range(1, count + 1):
+                        midpoints_array[i-1] = (x[i] + x[i-1])/2
+                        summ += func(midpoints_array[i-1])
+                    return summ*h/3
+                else:
+                    raise ValueError("Please enter positive numbers: "+str(count))
+            raise TypeError("One or more arguments is(are) not number(s)! Enter a Number")
+    raise AttributeError("OH!!! one of the arguments was forgotten, Check it!")
+"""
+def linear(x):
+    return 1
+print(trapezoid_method(linear, 0, 2, )
+"""
